e54251f18b679f00dcb8ad0ad3da018880ffb039
Sai Pranav Vangala <111426893+Alleyss@users.noreply.github.com>
2024-08-25 12:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app, origins=["https://bfhl-frontend-rhqklyu8h-sai-pranav-vangalas-projects.vercel.app"])
+CORS(app)
 @app.route('/bfhl', methods=['POST', 'GET'])
 def bfhl():
     if request.method == 'POST':
